c110669fb23d99924f15966d888fef418146c82e
WONGCH9 <wongch9@oocl.com>
2022-12-01 14:21:33 +0800

    test: added test should_say_fizz_when_count_off_given_multiple_of_3

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -18,5 +18,17 @@ public class FizzBuzzTest {
         assertEquals("1", actual);
     }
 
+    @Test
+    void should_say_fizz_when_count_off_given_multiple_of_3(){
+        //given
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        //when
+        String actual = fizzBuzz.countOff(3);
+
+        //then
+        assertEquals("Fizz", actual);
+    }
+
 
 }
