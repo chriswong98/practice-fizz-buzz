2bf164225e040de33238a5abd912ee9cd60c2bc9
WONGCH9 <wongch9@oocl.com>
2022-12-01 14:25:25 +0800

    feat: added count off when number is multiple of 3

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -3,6 +3,9 @@ package tdd.fizzbuzz;
 public class FizzBuzz {
 
     public String countOff(int n){
+        if (n %3 ==0){
+            return "Fizz";
+        }
         return Integer.toString(n);
     }
 
