d6370e7bb37fb1674e27c6aef6a4f93997a56456
WONGCH9 <wongch9@oocl.com>
2022-12-01 14:52:14 +0800

    feat: added count off when number is multiple of 5

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -3,11 +3,16 @@ package tdd.fizzbuzz;
 public class FizzBuzz {
 
     public static final String FIZZ = "Fizz";
+    public static final String BUZZ = "Buzz";
 
     public String countOff(int n){
         if (n %3 ==0){
             return FIZZ;
         }
+        else if(n%5 ==0){
+            return BUZZ;
+        }
+        
         return Integer.toString(n);
     }
 
