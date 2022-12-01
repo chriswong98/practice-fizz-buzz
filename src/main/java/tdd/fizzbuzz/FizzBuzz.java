8f66a6b4f089b123caa7786be787ba01c787f400
WONGCH9 <wongch9@oocl.com>
2022-12-01 15:02:43 +0800

    feat: added count off when number is multiple of 5 and 7

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -7,9 +7,13 @@ public class FizzBuzz {
     public static final String FIZZ_BUZZ = "FizzBuzz";
     public static final String WHIZZ = "Whizz";
     public static final String FIZZ_WHIZZ = "FizzWhizz";
+    public static final String BUZZ_WHIZZ = "BuzzWhizz";
 
     public String countOff(int n){
-        
+
+        if(n%5==0&& n%7==0){
+            return BUZZ_WHIZZ;
+        }
         if(n%3==0&& n%7==0){
             return FIZZ_WHIZZ;
         }
